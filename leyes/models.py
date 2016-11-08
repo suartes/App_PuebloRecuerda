35b516a0d791d10fdd4e1a2fe001f9a2e0ad8f54
suartes <ulisesuarez@gmail.com>
2016-11-08 14:28:20 -0600

    se añaden modelos de iniciativas

--- leyes/models.py
+++ leyes/models.py
@@ -7,10 +7,13 @@ from diputados.models import Diputado
 # Create your models here.
 
 class Iniciativa(models.Model):
-    num = models.CharField(max_length=200)
-    nombre = models.CharField(max_length=200)
-    turno = models.DateTimeField()
+    num = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    turno = models.TextField()
     comision = models.CharField(max_length=200)
-    resolutivos = models.CharField()
+    resolutivos = models.TextField()
     enlace = models.URLField()
-    diputado = models.ForeignKey(Diputado,null=True,blank=True, on_delete=models.CASCADE)
+    diputado = models.ForeignKey(Diputado,null=True,blank=True)
+
+
+
--- leyes/views.py
+++ leyes/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
 
-# Create your views here.
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import(
+	CreateView,
+	UpdateView,
+	DeleteView
+
+)
+
+from .models import Iniciativa
+
+
+class IniciativaList(ListView):
+	model = Iniciativa
+	#context_object_name = 'iniciativa'
+
+class IniciativaDetail(DetailView):
+	model =  Iniciativa
+
+class IniciativaCreation(CreateView):
+	model = Iniciativa
+	success_url = reverse_lazy('iniciativa:list')
+	fields = ['num', 'nombre', 'turno', 'comision', 'resolutivos', 'enlace','diputado']
+
+
+class IniciativaUpdate(UpdateView):
+	model = Iniciativa
+	success_url = reverse_lazy('iniciativa:list')
+	fields = ['num', 'nombre', 'turno', 'comision', 'resolutivos', 'enlace','diputado']
+
+class IniciativaDelete(DeleteView):
+	model = Iniciativa
+	success_url = reverse_lazy('iniciativa:list')
