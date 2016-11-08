10d7eb834eeed688df0c4f04e2ca12c73afa7101
ULISES SUAREZ <ulisesuarez@gmail.com>
2016-11-08 08:32:02 -0600

    se eliminan errores de sintaxis

--- leyes/models.py
+++ leyes/models.py
@@ -1,15 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from diputados.models import Diputado
 
 
 # Create your models here.
 
 class Iniciativa(models.Model):
-    num = models.CharField(max_length=200),
-    nombre = models.CharField(max_length=200),
-    turno = models.DateTimeField(),
-    comision = models.CharField(max_length=200),
-    resolutivos = models.CharField(),
+    num = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    turno = models.DateTimeField()
+    comision = models.CharField(max_length=200)
+    resolutivos = models.CharField()
     enlace = models.URLField()
-    diputado = models.CharField(max_length=200)
+    diputado = models.ForeignKey(Diputado,null=True,blank=True, on_delete=models.CASCADE)
