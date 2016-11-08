76276fd60337d30b45e960f3a08c2a6fb96581dd
Ulises Suárez Verde <ulisesuarez@intsoptec.com>
2016-11-08 03:09:54 -0600

    se crea modelo de leyes

--- leyes/models.py
+++ leyes/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class Iniciativa(models.Model):
+    num = models.CharField(max_length=200),
+    nombre = models.CharField(max_length=200),
+    turno = models.DateTimeField(),
+    comision = models.CharField(max_length=200),
+    resolutivos = models.CharField(),
+    enlace = models.URLField()
+    diputado = models.CharField(max_length=200)
