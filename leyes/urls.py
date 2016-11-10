3b7e4ca8a0b577eda7e2b9c09afb441b1260beb0
suartes <ulisesuarez@gmail.com>
2016-11-10 06:15:16 -0600

    de modifica index

--- leyes/urls.py
+++ leyes/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import index, iniciativa_view
 
 urlpatterns = (
-   url(r'^leyes$',index,name='index'),
+   url(r'^leyes/$',index,name='index'),
    url(r'^leyes/nuevo/$',iniciativa_view, name="iniciativa_crear"),
     #url(r'^leyes/nuevo$', IniciativaCreation.as_view(), name="iniciativa_new"),
     #url(r'^leyes/editar/(?P<pk>\d+)$', IniciativaUpdate.as_view(), name="iniciativa_update"),
